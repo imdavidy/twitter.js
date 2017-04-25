de16b4eb4158b1db11d5730acdf1ed9c69cc605d
Emily Grace Acres <acres.emilygrace@gmail.com>
2017-04-25 14:45:42 -0400

    added static router

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const tweetBank = require('../tweetBank');
+
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', {title: 'Twitter', tweets: tweets } );
 });
-
-router.get('/stylesheets/style.css', function (req, res) {
-  res.sendFile('/Users/Fullstack/Junior/twitter.js/public/stylesheets/style.css');
-});
+router.use(express.static('public'));
+// router.get('/stylesheets/style.css', function (req, res) {
+//   res.sendFile('/Users/Fullstack/Junior/twitter.js/public/stylesheets/style.css');
+// });
 
 module.exports = router;
