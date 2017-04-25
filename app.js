18958bd670527efd0168542d0f8da98ec8af0f8a
David Yoon <imdavidy@gmail.com>
2017-04-25 12:14:07 -0400

    complete non-persistent

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+const tweetBank = require('./tweetBank')
+
+var PORT = 3000;
+app.listen(PORT, function() {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache:true}); // point nunjucks to the proper directory for templates
+// nunjucks.render('index.html');
+
 app.use(function (req, res, next) {
     // do your logging here
     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
@@ -8,16 +21,31 @@ app.use(function (req, res, next) {
 
     next();
 });
-app.get('//', function(req, res) {
-  res.send('' +  req.url);
+app.get('/', function(req, res) {
+  // const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  const tweets = tweetBank.find();
+  res.render( 'index', {title: 'Tweets', tweets: tweets} );
+
 });
 
-var PORT = 3000;
-app.listen(PORT, function() {
-  console.log(`Server listening on port ${PORT}`)
-})
-app.post('/:id', function (req, res) {
-  console.log(req.body); // undefined if body-parser isn't used
-  workshops.push(req.body.title);
-  res.status(201).send(workshops.join('\n')); // 201 means 'created'
-});
+
+// app.post('/:id', function (req, res) {
+//   console.log(req.body); // undefined if body-parser isn't used
+//   workshops.push(req.body.title);
+//   res.status(201).send(workshops.join('\n')); // 201 means 'created'
+// });
+
+// in some file that is in the root directory of our application... how about app.js?
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+// nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+//    if(err) throw err;
+//     console.log(output);
+// });
